2514e46ee140e4effe09db6c34a3c04ef3316de6
Katsuya <katsuya.f0515@gmail.com>
2016-02-02 00:08:08 +0900

    server unix:/tmp/unicorn.sock

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,9 +4,9 @@ rails_env = ENV['RAILS_ENV'] || "development"
 worker_processes 2
 working_directory rails_root
 
-listen "#{rails_root}/tmp/sockets/unicorn.sock"
+listen "/tmp/sockets/unicorn.sock"
 #listen 8080
-pid "#{rails_root}/tmp/pids/unicorn.pid"
+pid "/tmp/pids/unicorn.pid"
 
 stderr_path "#{rails_root}/log/#{rails_env}_unicorn_error.log"
 stdout_path "#{rails_root}/log/#{rails_env}_unicorn.log"
