67aa875259fb692dd8a8d8c056908f28ad330915
Katsuya <katsuya.f0515@gmail.com>
2016-02-01 21:22:52 +0900

    fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,9 @@ set :repo_url, 'git@github.com:katsuya0515/Goinder.git'
 set :deploy_to, '/var/www/myapp'
 
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
- 
+ set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+set :unicorn_config_path, "#{release_path}/config/unicorn.rb"
+
 # nokogiri はシステムライブラリを使うため bundle install にオプションを指定する
 #set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
 #set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
