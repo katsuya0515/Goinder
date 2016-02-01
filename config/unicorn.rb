a0784f64e6948abe2b0c6c45165cc8fc2ff1569f
Katsuya <katsuya.f0515@gmail.com>
2016-02-01 22:13:30 +0900

    fixed 5

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,3 +11,7 @@ pid "#{rails_root}/tmp/pids/unicorn.pid"
 stderr_path "#{rails_root}/log/#{rails_env}_unicorn_error.log"
 stdout_path "#{rails_root}/log/#{rails_env}_unicorn.log"
 
+ before_exec do |server|
+      ENV['BUNDLE_GEMFILE'] = "#{root}/Gemfile"
+    end
+
